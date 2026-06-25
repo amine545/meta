606d480391485a2c86a2b9c5ba3da2dea764fbe5
amine545 <aminemrabet05@gmail.com>
2026-06-25 21:06:34 +0200

    Set up Little Lemon React app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders homepage text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const homepageText = screen.getByText(/homepage/i);
+  expect(homepageText).toBeInTheDocument();
 });
